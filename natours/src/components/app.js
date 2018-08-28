58545cc45f3171038103a80638b0f33c7e446faf
zfreedman <zfreedman92@gmail.com>
2018-08-28 13:01:08 -0700

    feat: Nuked previous src/ folder
    Ran npm install inside natours/
    Spun up new src/ folder

---
+++ natours/src/components/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  render = () => {
+    console.log(this);
+    return (
+      <div>
+        App goes here
+      </div>
+    );
+  }
+}
+
+export default App;
