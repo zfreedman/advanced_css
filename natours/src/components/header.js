b6f77d5e1df830c6934350ca94f90f6ab500116b
zfreedman <zfreedman92@gmail.com>
2018-08-29 14:02:50 -0700

    feat: Added a responsive <h1> to <Header />

--- natours/src/components/header.js
+++ natours/src/components/header.js
@@ -7,7 +7,21 @@ class Header extends React.Component {
     return (
       <header className="header">
         <div className="logo-box">
-          <img src={require("./img/logo-white.png")} alt="Logo" className="logo" />
+          <img
+            src={require("./img/logo-white.png")}
+            alt="Logo"
+            className="logo"
+          />
+        </div>
+
+        <div className="text-box">
+          <h1 className="heading-primary">
+            <span className="heading-primary-main">Outdoors</span>
+
+            <span className="heading-primary-sub">
+              is where life happens
+            </span>
+          </h1>
         </div>
       </header>
     );
