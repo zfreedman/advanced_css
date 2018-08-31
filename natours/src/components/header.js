f136d135f0326ea944cc03a331e326bfb7bed336
zfreedman <zfreedman92@gmail.com>
2018-08-31 11:33:38 -0700

    chore: Converted current codebase to BEM

--- natours/src/components/header.js
+++ natours/src/components/header.js
@@ -6,24 +6,28 @@ class Header extends React.Component {
   render () {
     return (
       <header className="header">
-        <div className="logo-box">
+        <div className="header__logo-box">
           <img
             src={require("./img/logo-white.png")}
             alt="Logo"
-            className="logo"
+            className="header__logo"
           />
         </div>
 
-        <div className="text-box">
+        <div className="header__text-box">
           <h1 className="heading-primary">
-            <span className="heading-primary-main">Outdoors</span>
+            <span className="heading-primary--main">Outdoors</span>
 
-            <span className="heading-primary-sub">
+            <span className="heading-primary--sub">
               is where life happens
             </span>
           </h1>
 
-          <a id="mybtn" href="#" className="btn btn-animated btn-white">
+          <a
+            id="mybtn"
+            href="#"
+            className="btn btn--animated btn--white"
+          >
             Discover our tours
           </a>
         </div>
