ee6de956840f465dd1dac2e472ee929748e0bb19
zfreedman <zfreedman92@gmail.com>
2018-09-06 20:13:22 -0700

    feat: Continued building out <Main />
    Refactored left/right blocks under <h2> into <MainLeft /> and <MainRight
    /> functional components

--- natours/src/components/main.js
+++ natours/src/components/main.js
@@ -1,23 +1,22 @@
 import React from "react";
 
+import MainLeft from "./mainLeft";
+import MainRight from "./mainRight";
+
 class Main extends React.Component {
   render () {
     return (
       <main>
         <section className="section-about">
-          <div className="u-center-text u-margin-bottom-8">
+          <div className="u-center-text u-margin-bottom-big">
             <h2 className="heading-secondary">
               Exciting tours for adventurous people
             </h2>
           </div>
 
           <div className="row">
-            <div className="col-1-of-2">
-              Text content
-            </div>
-            <div className="col-1-of-2">
-              Image composition
-            </div>
+            {MainLeft()}
+            {MainRight()}
           </div>          
         </section>
       </main>
---
+++ natours/src/components/mainRight.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MainRight () {
+  return (
+    <div className="col-1-of-2">
+      Image composition
+    </div>
+  );
+}
