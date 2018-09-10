98e0b05f67867f63e953fd6054d510542ea0016c
zfreedman <zfreedman92@gmail.com>
2018-09-09 17:14:34 -0700

    chore: Updated index.js to reflect renaming

--- natours/src/index.js
+++ natours/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/app";
-import "./styles/main.scss";
+import "./sass/main.scss";
 
 ReactDOM.render(
   <App />,
