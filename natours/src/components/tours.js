b61130d10d055bcb34f889a38330f472ec9ce06a
zfreedman <zfreedman92@gmail.com>
2018-09-11 13:25:55 -0700

    feat: Built out more of the DOM structure for <Tours />

--- natours/src/components/tours.js
+++ natours/src/components/tours.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 export default () => {
-  const tours = [1, 2, 3];
+  const tours = [
+    [
+      3,
+      "very easy",
+      2,
+      "sea explorer",
+      30,
+      "cozy hostels",
+    ],
+    [
+      7,
+      "medium",
+      6,
+      "forest hiker",
+      40,
+      "provided tents",
+    ],
+    [
+      5,
+      "hard",
+      3,
+      "snow adventurer",
+      15,
+      "provided tents",
+    ]
+  ].map(makeTour);
 
   return (
     <section className="section-tours">
@@ -11,26 +36,52 @@ export default () => {
         </h2>
       </div>
       <div className="row">
-        {tours.map(TourCard)}
+        {tours.map((e, i) => TourCard(e, i + 1))}
       </div>
     </section>
   );
 };
 
-const TourCard = (i) => {
+const TourCard = ({
+  days,
+  difficulty, 
+  guideCount,
+  heading,
+  peopleCount,
+  sleepWhere,
+}, i) => {
+
   const classes = ["back", "front"];
   const classMap = classes.reduce((acc, str) => {
     acc[str] = `card__side card__side--${str} card__side--${str}-${i}`;
     return acc;
   }, {});
 
-
   return (
     <div className="col-1-of-3">
       <div className="card">
         <div
           className={classMap["front"]}>
-          front
+          <div className={`card__picture card__picture--${i}`}>
+            &nbsp;
+          </div>
+
+          <h4 className={`card__heading`}>
+            <span
+              className={`card__heading-span card__heading-span--${i}`}>
+              {heading}
+            </span>
+          </h4>
+
+          <div className="card__details">
+            <ul>
+              <li>{days}</li>
+              <li>{peopleCount}</li>
+              <li>{guideCount}</li>
+              <li>{sleepWhere}</li>
+              <li>{difficulty}</li>
+            </ul>
+          </div>
         </div>
         <div className={classMap["back"]}>
           back
@@ -39,3 +90,23 @@ const TourCard = (i) => {
     </div>
   );
 };
+
+const makeTour = ([
+  days,
+  difficulty, 
+  guideCount,
+  heading,
+  peopleCount,
+  sleepWhere,
+]) => {
+  const peopleStr = peopleCount === 1 ? "person" : "people";
+
+  return {
+    days: `${days} day tour`,
+    difficulty: `Difficulty: ${difficulty.toLowerCase()}`,
+    guideCount: `${guideCount} guide${guideCount === 1 ? "" : "s"}`,
+    heading: `The ${heading}`,
+    peopleCount: `Up to ${peopleCount} ${peopleStr}`,
+    sleepWhere: `Sleep in: ${sleepWhere.toLowerCase()}`,
+  };
+}
