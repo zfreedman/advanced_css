e9b8f4cb56d94749f3528ee92a2d0e9e975d5395
zfreedman <zfreedman92@gmail.com>
2018-09-19 15:53:31 -0700

    feat: Added responsive design to popup

--- natours/src/components/popup.js
+++ natours/src/components/popup.js
@@ -5,6 +5,7 @@ export default () => (
     <div className="popup__content">
       <PopupLeft />
       <PopupRight />
+      <a href="#section-tours" className="popup__close">&times;</a>
     </div>
   </div>
 );
@@ -15,10 +16,11 @@ const PopupLeft = () => {
   return (
     <div className={`${className}`}>
       {
-        images.map(e =>(
+        images.map((e, i) =>(
           <img
+          // <div
             alt="Tour Photo"
-            className={"popup__img"}
+            className={`popup__img popup__img-${i}`}
             key={`${className}-${e}`}
             src={require(`../img/${e}.jpg`)}
           />
@@ -29,17 +31,18 @@ const PopupLeft = () => {
 };
 
 const PopupRight = () => {
+  // return <div className="popup__b">b</div>;
   return (
     <div className="popup__right">
-      <a href="#section-tours" className="popup__close">&times;</a>
+      {/* <a href="#section-tours" className="popup__close">&times;</a> */}
       <h2 className="heading-secondary u-margin-bottom-small">Start booking now</h2>
       
       <h3 className="heading-tertiary u-margin-bottom-small">
         Important &ndash; Please read these terms before booking
       </h3>
 
-      <p className="popup__text u-margin-bottom-medium">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio ut enim blandit volutpat maecenas volutpat blandit. Cursus sit amet dictum sit amet justo donec enim diam. Dictum non consectetur a erat nam. Aliquet nec ullamcorper sit amet risus nullam eget felis eget. Sed augue lacus viverra vitae congue eu consequat ac. Viverra maecenas accumsan lacus vel. Mattis molestie a iaculis at. Mi quis hendrerit dolor magna eget. Nec feugiat in fermentum posuere urna nec tincidunt praesent semper. Nulla aliquet porttitor lacus luctus. Amet mattis vulputate enim nulla aliquet porttitor lacus luctus. Duis at consectetur lorem donec massa sapien. Lorem dolor sed viverra ipsum nunc aliquet bibendum enim. Congue nisi vitae suscipit tellus mauris a diam. Nunc consequat interdum varius sit amet mattis vulputate enim nulla. Odio tempor orci dapibus ultrices in iaculis. Ornare massa eget egestas purus viverra accumsan in.
+      <p className="popup__text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio ut enim blandit volutpat maecenas volutpat blandit. Cursus sit amet dictum sit amet justo donec enim diam. Dictum non consectetur a erat nam. Aliquet nec ullamcorper sit amet risus nullam eget felis eget. Sed augue lacus viverra vitae congue eu consequat ac. Viverra maecenas accumsan lacus vel. Mattis molestie a iaculis at. Mi quis hendrerit dolor magna eget. Nec feugiat in fermentum posuere urna nec tincidunt praesent semper. Nulla aliquet porttitor lacus luctus. Amet mattis vulputate enim nulla aliquet porttitor lacus luctus. Duis at consectetur lorem donec massa sapien. Lorem dolor sed viverra ipsum nunc aliquet bibendum enim. Congue nisi vitae suscipit tellus mauris a diam. Nunc consequat interdum varius sit amet mattis vulputate enim nulla.
       </p>
 
       <a href="#" className="btn btn--green">Book now</a>
