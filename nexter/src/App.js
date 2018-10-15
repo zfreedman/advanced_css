d443e72e4ea482bd0f4e11dc587d963c1882d5da
Zach Freedman <zfreedman92@gmail.com>
2018-10-15 16:42:48 -0700

    chore: Initial commit
    Grabbed assets from previous project (webpack, config, package.json)
    Grabbed starter assets for this project (img, sass)

---
+++ nexter/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      text here
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ nexter/src/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import "./sass/main.scss";
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('container')
+);
