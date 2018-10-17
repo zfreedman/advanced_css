53341f00ccdf3b511ab2befc5ec99e9ef891cdea
Zach Freedman <zfreedman92@gmail.com>
2018-10-17 12:35:49 -0700

    feat: Built out <StoryPictures />

--- nexter/src/components/StoryPictures.js
+++ nexter/src/components/StoryPictures.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from 'react';
 export default function StoryPictures() {
   return (
     <div className="story__pictures">
-      story pictures
+      <img
+        alt="Couple with new house"
+        className="story__img--1"
+        src={require("../img/story-1.jpeg")}
+      />
+
+      <img
+        alt="Couple with new house"
+        className="story__img--2"
+        src={require("../img/story-2.jpeg")}
+      />
     </div>
   );
 };
